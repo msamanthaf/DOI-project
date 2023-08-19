1246fce88ebb51299d35b95c2184fa39da30e3fb
Flayre <mariesamantha.f@gmail.com>
2023-08-19 16:53:14 -0700

    Implemented stable marriage python algorithm

--- client/src/json.js
+++ client/src/json.js
@@ -1,4 +1,5 @@
 export const json = {
 	surveyId: "feb953fa-1092-4b22-a047-da393672fac3",
 	surveyPostId: "1cfe6c04-f0c2-42a4-964e-2b7833275b5f",
+	clientId: "e2bd506a-a218-4f5c-b8ae-768f203e399b",
 };
