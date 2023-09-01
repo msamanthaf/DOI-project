1037de9556d1211c49e84642f7bbd9d63a50839b
Michael Tuntang <mike.tuntang547@gmail.com>
2023-08-31 22:43:26 -0700

    added routes and links to Join Now button connecting to SurveyComponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import './App.css';
 import SurveyComponent from './pages/Survey/SurveyComponent.jsx';
 import Navbar from './components/Navbar/Navbar';
@@ -5,51 +7,48 @@ import doiLogo from './assets/doi.svg';
 import doiImage from './assets/people-art.png';
 
 const navItems = [
-	{ name: 'About', link: '#home' },
-	{ name: 'Process', link: '#about' },
-	{ name: 'Algorithm', link: '#contact' },
-	{ name: 'FAQ', link: '#contact' },
+	{ name: 'About', link: '/about' },
+	{ name: 'Process', link: '/process' },
+	{ name: 'Algorithm', link: '/algorithm' },
+	{ name: 'FAQ', link: '/faq' },
 ];
 
 function App() {
 	return (
-		<div className="bg-doi-blue h-screen w-screen">
-			<div className="flex flex-row justify-between pt-16 pb-2 px-28">
-				<img
-					src={doiLogo}
-					alt="doi Logo"
-					style={{
-						maxWidth: '3%',
-					}}
-				/>
-				<Navbar navItems={navItems} />
-				<button className="bg-white text-doi-blue text-base rounded-lg font-sans font-bold py-0.5 px-4">
-					Join Now
-				</button>
-			</div>
-			<div className="flex flex-row justify-items-center my-32 mx-72">
-				<img src={doiImage} alt="DOI Image" className="w-1/2" />
-				<div className="flex flex-col m-5">
-					<div className="mt-32">
-						<text className="font-bold font-mufteya text-8xl text-doi-light-blue">
-							Date 🤝 Data
-						</text>
-					</div>
-					<div className="mt-10">
-						<text className="font-plex text-xl text-doi-light-blue">
-							DOI (Dating Orang Indo) Project is a one-off, student-run passion
-							project aiming to connect Indonesian students studying in Canada
-							(*Beta release: Only in Vancouver) to meet their ideal date from
-							your city/university.
-						</text>
-					</div>
+		<Router>
+			<div className="bg-doi-blue h-screen w-screen">
+				<div className="flex flex-row justify-between pt-16 pb-2 px-28">
+					<img src={doiLogo} alt="doi Logo" style={{ maxWidth: '3%' }} />
+					<Navbar navItems={navItems} />
+					<button className="bg-white text-doi-blue text-base rounded-lg font-sans font-bold py-0.5 px-4">
+						<Link to="/survey" className="block">
+							Join Now
+						</Link>
+					</button>
+				</div>
+				<div>
+					<Routes>
+						<Route
+							path="/about"
+							element={<div>About Component or Placeholder</div>}
+						/>
+						<Route
+							path="/process"
+							element={<div>Process Component or Placeholder</div>}
+						/>
+						<Route
+							path="/algorithm"
+							element={<div>Algorithm Component or Placeholder</div>}
+						/>
+						<Route
+							path="/faq"
+							element={<div>FAQ Component or Placeholder</div>}
+						/>
+						<Route path="/survey" element={<SurveyComponent />} />
+					</Routes>
 				</div>
 			</div>
-
-			{/* <div>
-				<SurveyComponent></SurveyComponent>
-			</div> */}
-		</div>
+		</Router>
 	);
 }
 
