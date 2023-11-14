1cc779e208a240e231a21ad3d036ed5f109baa54
mtuntang <mtuntang@students.cs.ubc.ca>
2023-11-14 12:53:51 -0800

    integrated flowbite

--- web/doi-web/tailwind.config.ts
+++ web/doi-web/tailwind.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
 	mode: 'jit',
 	content: [
+		"./node_modules/flowbite-react/**/*.js",
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
@@ -16,6 +17,8 @@ const config: Config = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [
+		require("flowbite/plugin")
+	],
 };
 export default config;
