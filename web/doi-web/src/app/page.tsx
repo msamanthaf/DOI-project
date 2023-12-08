33db16777501a5cd1be40cacfc7368dd88a9db72
Michael Tuntang <mike.tuntang547@gmail.com>
2023-12-07 21:31:34 -0800

    styling fixes and clean up

--- web/doi-web/src/app/page.tsx
+++ web/doi-web/src/app/page.tsx
@@ -3,22 +3,22 @@ import { Header } from '@/components/Header';
 
 export default function Home() {
 	const menuItems = [
-		{ name: 'Home', href: '#' },
-		{ name: 'Products', href: '/products' },
-		{ name: 'About', href: '/about' },
-		{ name: 'Contact', href: '/contact' },
+		{ name: 'Join Now', href: '#' },
+		{ name: 'About', href: '#' },
+		{ name: 'Process', href: '#' },
+		{ name: 'Algorithm', href: '#' },
+		{ name: 'FAQ', href: '#' },
+		{ name: 'The Creators', href: '#' },
+		{ name: 'Become a Sponsor', href: '#' },
 	];
 
 	return (
 		<div>
 			<Header menuItems={menuItems} />
 			<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<section id="profile"></section>
-			<section id="profile"></section>
-			<section id="profile"></section>
-			<section id="profile"></section>
-			<section id="profile"></section>
-			<section id="profile"></section>
+				{menuItems.map((item, index) => (
+					<section key={index} id={item.name}></section>
+				))}
 			</main>
 		</div>
 	);
--- web/doi-web/src/components/Header.tsx
+++ web/doi-web/src/components/Header.tsx
@@ -1,142 +1,93 @@
-"use client";
-import React, { useState, useEffect } from "react";
-import logo from "../../public/doi-logo.png";
+'use client';
+import React, { useState, useEffect } from 'react';
+import logo from '../../public/doi-logo.png';
 
 interface HeaderProps {
-  menuItems: { name: string; href: string }[];
+	menuItems: { name: string; href: string }[];
 }
 
 export const Header: React.FC<HeaderProps> = ({ menuItems }) => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
+	const [isDropdownOpen, setIsDropdownOpen] = useState(true);
+	const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
 
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth >= 1157) {
-        setIsDropdownOpen(true);
-      }
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth >= 1024) {
+				setIsDropdownOpen(true);
+			}
 
-      if (window.innerWidth < 1157) {
-        setIsDropdownOpen(false);
-      }
-    };
+			if (window.innerWidth < 1024) {
+				setIsDropdownOpen(false);
+			}
+		};
 
-    window.addEventListener("resize", handleResize);
+		window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+		return () => window.removeEventListener('resize', handleResize);
+	}, []);
 
-  return (
-    <div className="fixed flex items-center left-14 right-14">
-      <nav
-        className="bg-navbar bg-opacity-50 backdrop-filter backdrop-blur-md border-gray-200 dark:bg-gray-900 flex flex-wrap items-center justify-between mx-auto rounded-b-2xl"
-        style={{ width: "90vw" }}
-      >
-        <div className="flex flex-wrap items-center justify-between mx-auto p-4 bg-EBE9F7">
-          <img
-            src="/doi-logo.png"
-            alt="DOI Logo"
-            className="h-12 mr-5 self-center"
-          />
-          <button
-            data-collapse-toggle="navbar-default"
-            type="button"
-            className="xl:hidden inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-default"
-            aria-expanded={isDropdownOpen}
-            onClick={toggleDropdown}
-          >
-            <span className="sr-only">Open main menu</span>
-            <svg
-              className="w-5 h-5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 17 14"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M1 1h15M1 7h15M1 13h15"
-              />
-            </svg>
-          </button>
-          {isDropdownOpen && (
-            <div className="w-full md:block md:w-auto" id="navbar-default">
-              <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 ml-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 :border-0">
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                    aria-current="page"
-                  >
-                    Join Now
-                  </a>
-                </li>
-                <li>|</li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Process
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Algorithm
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    FAQ
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    The Creators
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                  >
-                    Become a Sponsor
-                  </a>
-                </li>
-                <li>|</li>
-                <a href="#" className="self-center ml-[-5] bg-transparent">
-                  <img
-                    src="/insta-icon.png"
-                    alt="DOI Logo"
-                    className="h-5 self-center"
-                  />
-                </a>
-              </ul>
-            </div>
-          )}
-        </div>
-      </nav>
-    </div>
-  );
+	return (
+		<div className="fixed flex items-center left-14 right-14">
+			<nav
+				className="bg-navbar bg-opacity-50 backdrop-filter backdrop-blur-md border-gray-200 dark:bg-gray-900 flex flex-wrap items-center justify-between mx-auto rounded-b-2xl"
+				style={{ width: '90vw' }}
+			>
+				<div className="flex flex-wrap items-center justify-between mx-auto p-4 bg-EBE9F7">
+					<img
+						src="/doi-logo.png"
+						alt="DOI Logo"
+						className="h-12 mr-5 self-center"
+					/>
+					<button
+						data-collapse-toggle="navbar-default"
+						type="button"
+						className="lg:hidden inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+						aria-controls="navbar-default"
+						aria-expanded={isDropdownOpen}
+						onClick={toggleDropdown}
+					>
+						<span className="sr-only">Open main menu</span>
+						<svg
+							className="w-5 h-5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 17 14"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M1 1h15M1 7h15M1 13h15"
+							/>
+						</svg>
+					</button>
+					{isDropdownOpen && (
+						<div className="w-full md:block md:w-auto" id="navbar-default">
+							<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 ml-4 border border-gray-100 rounded-lg lg:flex-row lg:space-x-8 rtl:space-x-reverse lg:mt-0 lg:border-0 items-center">
+								{menuItems.map((item, index) => (
+									<li key={index}>
+										<a
+											href={item.href}
+											className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+										>
+											{item.name}
+										</a>
+									</li>
+								))}
+								<a href="#" className="self-center ml-[-5] bg-transparent">
+									<img
+										src="/insta-icon.png"
+										alt="DOI Logo"
+										className="h-5 self-center"
+									/>
+								</a>
+							</ul>
+						</div>
+					)}
+				</div>
+			</nav>
+		</div>
+	);
 };
