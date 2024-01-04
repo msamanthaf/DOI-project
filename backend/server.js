28d45720620b32769ba905ba9ec4c9599b13fbc4
Michael Tuntang <mike.tuntang547@gmail.com>
2024-01-04 02:41:08 -0800

    added main server file and batchRoutes

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use('/api', batchRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
+});
